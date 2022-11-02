929c27f98693da61ce7452dce5a575f044d328e0
Artur Zaborskii <art.zabor1988@yandex.ru>
2022-11-02 10:20:01 +0300

    Django ORM

---
+++ yatube/posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('groups/', views.group_list),
+    path('group/<slug:slug>/', views.group_posts),
+]
---
+++ yatube/posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def index(request):
+    pass
+
+
+def group_list(request):
+    pass
+
+
+def group_posts(request):
+    pass
